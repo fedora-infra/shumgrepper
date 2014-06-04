652e6214f7474df5188cf38a9b58e9f9e215e0f3
charulagrl <charul.agrl@gmail.com>
2014-06-04 13:55:42 +0530

    added /md5, /sha256, /tar endpoints

--- shumgrepper/app.py
+++ shumgrepper/app.py
@@ -22,3 +22,30 @@ def sha1sum(sha1):
 
     return flask.render_template('files.html', all_files=message)
 
+
+# request files by md5sum
+@app.route('/md5/<md5>')
+@app.route('/md5/<md5>')
+def md5sum(md5):
+    message = sm.File.by_md5(session, md5)
+
+    return flask.render_template('files.html', all_files=message)
+
+
+# request files by sha256sum
+@app.route('/sha256/<sha256>')
+@app.route('/sha256/<sha256>')
+def sha256sum(sha256):
+    message = sm.File.by_sha256(session, sha256)
+
+    return flask.render_template('files.html', all_files=message)
+
+
+# request files by tarsum
+@app.route('/tar/<tar>')
+@app.route('/tar/<tar>')
+def tar_sum(tar):
+    message = sm.File.by_tar_sum(session, tar)
+
+    return flask.render_template('files.html', all_files=message)
+
