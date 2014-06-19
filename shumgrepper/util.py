c124fd71cd1e7460e8fe022ab42669068bd446c4
charulagrl <charul.agrl@gmail.com>
2014-06-19 17:46:12 +0530

    contains definition for function common_files

--- shumgrepper/util.py
+++ shumgrepper/util.py
@@ -2,7 +2,7 @@ import json
 import flask
 
 def JSONEncoder(messages):
-    " Serialize collection in each message"
+    ''' Serialize collection in each message '''
     msg_list = []
     for message in messages:
         new_dict = dict(
@@ -21,4 +21,22 @@ def request_wants_html():
     best = flask.request.accept_mimetypes \
         .best_match(['application/json', 'text/html', 'text/plain'])
     return best == 'text/html'
+
+def common_files(messages_list):
+    '''return common filenames present in packages'''
+    final_list = []
+    for messages in messages_list:
+        sha1sum_list = []
+        for msg in messages:
+            sha1sum_list.append(msg.sha1sum)
+        final_list.append(sha1sum_list)
+
+    common_sha1 = set(final_list[0]).intersection(*final_list)
+    common_filenames = []
+    for sha1sum in common_sha1:
+        for msg in messages:
+            if sha1sum == msg.sha1sum:
+                common_filenames.append(msg.filename)
+
+    return common_filenames
 
