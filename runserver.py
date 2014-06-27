fdaf901672b40a2e2540452620a214a0675050cc
charulagrl <charul.agrl@gmail.com>
2014-06-28 03:00:00 +0530

    changing imports

--- runserver.py
+++ runserver.py
@@ -3,5 +3,5 @@ import os
 if not os.environ.get("SHUMGREPPER_CONFIG"):
     os.environ["SHUMGREPPER_CONFIG"] = '../development.cfg'
 
-from shumgrepper.app import app
+from shumgrepper import app
 app.run()
