7ae6a809a3019b0e32675274f2aac3c78a7f5712
charul <charul.agrl@gmail.com>
2015-04-21 21:57:05 +0530

    Add comments for why [0][1:] is used in api_tarball_filenames

--- shumgrepper/api.py
+++ shumgrepper/api.py
@@ -81,8 +81,12 @@ def api_tar_sum(tar_sum):
 # request files by tarball
 @app.route('/api/tarball/<tarball>/filenames')
 def api_tarball_filenames(tarball):
+    # returns a list of tuples
     filenames = sm.File.tarball_filenames(session, tarball)
 
+    # [0] to get the first element of tuple i.e. filename
+    # [1:] to remove '/' from filename 0th position
+    # This will allow querying by filename and get file data
     filename_list = [filename[0][1:] for filename in filenames]
 
     return flask.Response(
